89283246e53cc4812b367645c662126cff15c01b
Facu Moyano <facujoel1997@hotmail.com>
2022-04-24 19:06:01 -0300

    provider

--- src/App.js
+++ src/App.js
@@ -1,56 +1,24 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
-
 import { NavBar } from "./components/NavBar/NavBar";
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import { Inicio } from './components/Inicio/Inicio';
-
 import { ItemListContainer } from "./components/ItemListContainer/ItemListContainer";
 import { ItemDetailContainer } from './components/ItemDetailContainer/ItemDetailContainer';
-import {CartContext} from './components/CartContext/CartContext';
-import { useState } from 'react';
+import {CartProvider} from './components/CartContext/CartContext';
+
 // import {Footer} from './components/Footer/Footer';
-
-
-
-
 
 
 function App() {
 
-  const [cart, setCart] = useState([])
-
-  const agregarAlCarrito = (item)=>{
-    setCart([...cart, item])
-  }
-
-  const isInCart = (id) => {
-    return cart.find((prod) => prod.id === id )
-  }
-
-  const cantidadCart = ()=>{
-    return cart.reduce((acc, prod) => acc + prod.cantidad, 0)
-  }
-
-  const totalCart = () =>{
-    return cart.reduce((acc, prod) => acc + prod.cantidad * prod.precio, 0)
-  }
+ 
 
   return (
 
-    <CartContext.Provider value={{
-      cart,
-      agregarAlCarrito,
-      isInCart,
-      cantidadCart,
-      totalCart,
-    }} >
+    <CartProvider>
     
     <BrowserRouter>
-
- 
       <NavBar/>
-
-
 
       <Routes>
         
@@ -60,24 +28,15 @@ function App() {
         <Route path='/productos/:catId' element= {<ItemListContainer/>}/>
         <Route path='/Nacionales/:catId' element= {<ItemListContainer/>}/>
         <Route path='/roductos/:catId' element= {<ItemListContainer/>}/>
-        
-        
-       
-        
-        
         <Route path='*' element= {<Navigate to= '/' />}/>
 
       </Routes>
-
-      
       {/* <Footer/> */}
-      
-
     </BrowserRouter>
       
 
-    </CartContext.Provider>
-    
+    </CartProvider>
+  
   );
 }
 
